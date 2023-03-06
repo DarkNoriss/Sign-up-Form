482b2c815ef4658a87fa1d551aabc926675f95b8
DarkNoriss <darknoriss@gmail.com>
2023-03-06 05:38:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import "./style.scss";
+
+const form = document.getElementById("myForm");
+form.addEventListener("submit", submitForm);
+
+const warningText = "* Password do not match";
+const warningP = document.getElementById("warning");
+
+function submitForm(event) {
+  event.preventDefault();
+
+  let firstName = event.target["first-name"].value;
+  let lastName = event.target["last-name"].value;
+  let email = event.target["email"].value;
+  let phoneNumber = event.target["phone-number"].value;
+  let password = event.target["password"].value;
+  let confirmPassword = event.target["confirm-password"].value;
+
+  if (password != confirmPassword) {
+    warningP.innerText = warningText;
+  } else {
+    warningP.innerText = "";
+    form.reset();
+  }
+}
